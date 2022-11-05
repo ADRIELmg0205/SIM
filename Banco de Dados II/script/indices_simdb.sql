4b52d691495e9efa8ef3ea0ae48a51647f58e3d5
ADRIELmg0205 <adrielgraupp0205@gmail.com>
2022-11-05 16:00:53 -0300

    criando views e indices

---
+++ Banco de Dados II/script/indices_simdb.sql
@@ -0,0 +1,14 @@
+-- criando indice para nome/razao social do cliente
+create index clf_nomclf_sk
+on clientefisico(nomclf);
+
+create index clj_razclj_sk
+on clientejuridico(razclj);
+
+-- indice para nome do funcinario
+create index fun_nomfun_sk
+on funcionario(nomfun);
+
+-- indice para nome do vendedor
+create index ven_nomven_sk
+on vendedor(nomven);
---
+++ Banco de Dados II/script/views_simdb.sql
@@ -0,0 +1,18 @@
+-- criando view para mostrar nome do cliente que esta realizando a compra
+create view nomclf_vw as
+select nomclf from clientefisico cf
+inner join cliente c on c.cpfclf = cf.cpfclf 
+inner join venda v on c.codcli = v.codcli;
+
+-- criando view para mostrar contato dos fornecedores cujam são de Santa Catarina
+create view contato_fornecedores_sc_vw as
+select telfor, emafor from fornecedor f
+inner join endereco e on e.cepend = f.cepend 
+where e.estend = 'Santa Catarina';
+
+-- Criando view para mostrar cliente juridicos (razão social) que residam no Sul
+create view razclj_clientesdosul_vw as
+select razclj from clientejuridico cj
+inner join endereco e on cj.cepend = e.cepend 
+where e.estend = 'Santa Catarina' or e.estend = 'Parana'
+or e.estend = 'Rio Grande do Sul';
